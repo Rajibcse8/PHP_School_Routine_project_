b32fb3ca70d8505042cd7c8281ca672eb05045c0
Rajibcse8 <73878397+Rajibcse8@users.noreply.github.com>
2021-01-07 03:49:11 +0600

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,6 +1,8 @@
 
 <?php
 
+
+   
 $conn= mysqli_connect('localhost','root','','routine');
  
 
@@ -27,12 +29,16 @@ if(isset($_POST['button1'])){
         VALUES('$year2','$class2','$section2','$day2','$period2','$time2','$subject2','$teacher2')";
 
         $create_query= mysqli_query($conn,$query);
- 
    
-}
+        }
 
-}
+ }
+
+
 ?>
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -43,14 +49,14 @@ if(isset($_POST['button1'])){
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Routine_admin</title>
   </head>
   <body>
 
 
   <div class="container"  >
           
-          <form action= ""   method="post">
+          <form action= "admin.php"   method="post">
                 
                 <input class="form-control" type="number"  name="year1" placeholder="year">
                <br>
@@ -74,88 +80,84 @@ if(isset($_POST['button1'])){
           </form>
      
   </div>
-  <!--udpade delete design part-->
+  <!--udpade delete design part  and start -->
      
-  <div class="container  shadow m-5 p-3" class="d-flex justify-content-around">
-          
-          <form action= ""   method="post">
-                
-                <?php 
-                
-                     if(isset($_GET['update'])){
+   <div class="container m-3 p-5">
 
-                      $stid =$_GET['update'];
-                      $query="SELECT * FROM  six WHERE id={$stid}";
-                      $make_upd= mysqli_query($conn,$query);
-                      while($rd=mysqli_fetch_assoc($make_upd)){
+    <form action="" method="post"  class="d-flex justify-content-around">
+    
+       <?php 
 
+        if(isset($_GET['update'])){
+          $ii=$_GET['update'];
+              
+              $q1="SELECT * FROM six WHERE id= $ii ";
+              $q2=mysqli_query($conn,$q1);
+              $q3=mysqli_fetch_assoc($q2);
 
-                        $upyear=$rd['year'];
-                        $upclass=$rd['class'];
-                        $upsec=$rd['section'];
-                        $upday=$rd['day'];
-                        $upnumer=$rd['pnumber'];
-                        $upt=$rd['timee'];
-                        $upsub=$rd['sname'];
-                        $upt=$rd['tname'];
-                        $id1=$rd['id'];
-                
-                ?>
-
-               <input class="form-control" type="number"name="id"  value=<?php  echo $id1;  ?>  >
-               <input class="form-control" type="number"name="year1"  value=<?php  echo $upyear;  ?>  >
-               <input class="form-control" type="text"  name="class1"   value=<?php   echo $upclass;    ?>  >
-               <input class="form-control" type="text"  name="section1" value =<?php  echo $upsec;  ?>  >
-               <input class="form-control" type="text"  name="day1"     value=<?php echo $upday;  ?>  >
-               <input class="form-control" type="text"  name="period1"  value=<?php echo $upnumer  ?> >
-               <input class="form-control" type="text"  name="time1"    value =<?php  echo $upt; ?>>
-               <input class="form-control" type="text"  name="subject1" value=<?php echo $upsub;  ?> > 
-               <input class="form-control" type="text"  name="teacher1" value=<?php  echo $upt;  ?> >
-               <input type="submit" value="update" name="button2" class="btn  btn-primary" >
-
-
-             <?php }}  ?>
-
-
-             <?php 
-             
-                  if(isset($_POST['button2'])){
-
-                    $id1=$_POST['id'];
-                    $year2= $_POST['year1'];
-                    $class2=$_POST['class1'];
-                    $section2=$_POST['section1'];
-                    $day2=$_POST['day1'];
-                    $period2=$_POST['period1'];
-                    $time2=$_POST['time1'];
-                    $subject2=$_POST['subject1'];
-                    $teacher2=$_POST['teacher1'];
- 
- if(!empty($year2) && !empty($class2) && !empty($section2) && !empty($period2) &&!empty($time2) &&!empty($subject2) && !empty($teacher2)){
-               
-   $query="UPDATE TABLE six SET year=$year2,class=$class2,section=$section2,day=$day2,pnumber=$period2,timee=$time2,sname=$subject2 , tname= $teacher2 WHERE id=$id1 ";
        
-         
-          $updateQuery= mysqli_query($conn, $query); 
-         
-         
+       ?>
 
-   //    header("Refresh:0; url=admin.php");
+             <input class="form-control" type="number"  name="year1" value="<?php  echo $q3['year'];  ?>" >
+              <br>
+              <input class="form-control" type="text"  name="class1" value="<?php  echo $q3['class'];  ?>">
+              <br>
+              <input class="form-control" type="text"  name="section1" value="<?php  echo $q3['section'];  ?>">
+              <br>
+              <input class="form-control" type="text"  name="day1" value="<?php  echo $q3['day'];  ?>" >
+              <br>
+              <input class="form-control" type="text"  name="period1" value="<?php  echo $q3['pnumber'];  ?>">
+              <br>
+              <input class="form-control" type="text"  name="time1" value="<?php  echo $q3['timee'];  ?>">
+              <br>
+              <input class="form-control" type="text"  name="subject1" value="<?php  echo $q3['sname'];  ?>">
+              <br>
+              <input class="form-control" type="text"  name="teacher1" value="<?php  echo $q3['tname'];  ?>">
+              <br>
+              <input type="submit" value="send" name="button2" class="btn  btn-info" >
+
+            <?php
+            
+            
+                      if(isset($_POST['button2'])){
+                         $iddd=$q3['id'];
+                        $pclas=$_POST['class1'];
+                        $psec=$_POST['section1'];
+                        $pday= $_POST['day1'];
+                        $pnum= $_POST['period1'];
+                        $ptm= $_POST['time1'];
+                        $psub=$_POST['subject1'];
+                        $ptec=$_POST['teacher1'];
+                        
+                         $q4="UPDATE six SET class='$pclas' , section= '$psec' , day='$pday' ,
+                         pnumber ='$pnum' , timee='$ptm' , sname='$psub', tname= '$ptec'
+                         WHERE id= $iddd";
+
+                         $q5=mysqli_query($conn,$q4);
+
+                         header("Refresh:0; url=admin.php");
+
+                      }
+            
+            ?>
+
+
+       
+       <?php    } ?>
+    
+    </form>
    
-} 
-
-                  }
-             
-             ?>
-
-     </form>
-     
-  </div>
+   
+   
+   </div>
+      
 
 
   <!-- update  part  end -->
 
 
+
+<!-- show data part start                                           -->
   <div class="container">
          <table class="table table-bordered">
          <tr>
@@ -190,6 +192,10 @@ if(isset($_POST['button1'])){
                     $Subject=$rd['sname'];
                     $Teacher=$rd['tname'];
                        
+                   
+
+               
+         
          
          
          ?>
@@ -202,9 +208,10 @@ if(isset($_POST['button1'])){
             <td> <?php  echo $Time;  ?> </td>
             <td> <?php  echo  $Subject;  ?> </td>
             <td> <?php  echo $Teacher;  ?> </td>
-            <td>  <a href="admin.php?update=<?php  echo $ID;    ?>    " class="btn btn-info">Update</a> </td>
-            <td>  <a href="admin.php?delete=<?php  echo $ID;    ?>    " class="btn btn-danger">Delete</a> </td>
-                
+            <td>  <a href="admin.php?update=<?php  echo $ID;?>" class="btn btn-info">Update</a> </td>
+            <td>  <a href="admin.php?delete=<?php  echo $ID; ?>" class="btn btn-danger">Delete</a> </td>
+            
+         
          </tr>
 
          <?php  }} else{
@@ -213,6 +220,7 @@ if(isset($_POST['button1'])){
          } ?>
          
          </table>
+
 <!-- DELETE QUERY START -->
          <?php 
 
@@ -222,21 +230,27 @@ if(isset($_POST['button1'])){
         $stdid=$_GET['delete'];
         $query ="DELETE FROM  six WHERE id={$stdid }";
         $make_del=mysqli_query($conn,$query);
-        header("Refresh:0; url=admin.php");
+       
+        //header("Refresh:0; url=admin.php");
           
       }
 
 ?>
+  
+<!-- DELETE QUERY START -->  
+
 
 <!-- Update QUery Start  -->
 
-    <?php
-    
-    
-    ?>
+
 
   </div>
     
+
+<!--         show data end ...............................................................                  -->
+
+
+
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
